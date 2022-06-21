ff3a0c50f561d93ac1039f77ebf788fc7f89b607
Matthew Liem <mattliem@amazon.com>
2022-06-21 13:04:37 -0700

    adding trino autoscaling

---
+++ applications/trino-autoscaling/trino_cloudwatch_ba.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x -e
+
+REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
+echo "$REGION" >/tmp/aws-region
+
+cd /home/hadoop
+aws s3 cp s3://<bucket>/trino_cloudwatch.sh .
+chmod +x trino_cloudwatch.sh
+
+echo '* * * * * sudo /bin/bash -l -c "/home/hadoop/trino_cloudwatch.sh; sleep 30; /home/hadoop/trino_cloudwatch.sh"' >crontab.txt
+
+crontab crontab.txt
