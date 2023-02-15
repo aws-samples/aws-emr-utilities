dd0c6eb40a99ef35826986edc69c070657b7ef1c
knkarthik01 <95832419+knkarthik01@users.noreply.github.com>
2023-02-15 09:05:37 -0700

    Update sparkhistory.sh

--- utilities/self-managed-spark-history-server-emr/code/sparkhistory.sh
+++ utilities/self-managed-spark-history-server-emr/code/sparkhistory.sh
@@ -1,7 +1,7 @@
 sudo ln -s `ls /usr/share/aws/emr/emrfs/lib/emrfs-hadoop-assembly*` /usr/lib/spark/jars/emrfs.jar
 sudo ln -s `ls /usr/lib/hadoop/hadoop-aws-*` /usr/lib/spark/jars/hadoop-aws.jar
-sudo ln -s `ls /usr/share/aws/aws-java-sdk/aws-java-sdk-core-*` /usr/lib/spark/jars/aws-java-sdk-core.jar
-sudo ln -s `ls /usr/share/aws/aws-java-sdk/aws-java-sdk-s3-*` /usr/lib/spark/jars/aws-java-sdk-s3.jar
-aws s3api put-object —bucket <yourbucketname> —key sparkhistory/
+sudo ln -s `ls /usr/share/aws/aws-java-sdk/aws-java-sdk-bundle-*` /usr/share/aws/aws-java-sdk/aws-java-sdk-bundle.jar
+
+aws s3api put-object —-bucket <yourbucketname> —-key sparkhistory/
 sudo systemctl stop spark-history-server
 sudo systemctl start spark-history-server
