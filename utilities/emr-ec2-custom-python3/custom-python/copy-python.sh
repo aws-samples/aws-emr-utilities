1867a157470e567b964ac038a85a06cee4ff4b25
Damon P. Cortesi <d.lifehacker@gmail.com>
2023-06-01 11:24:29 -0700

    A few minor updates and add doc on how to reuse python archive

---
+++ utilities/emr-ec2-custom-python3/custom-python/copy-python.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+PYTHON_ARCHIVE=$1
+
+# Copy and extract the Python archive into `/usr/local`
+sudo aws s3 cp ${PYTHON_ARCHIVE} /usr/local/
+cd /usr/local/
+sudo tar -xf $(basename ${PYTHON_ARCHIVE}) && rm $(basename ${PYTHON_ARCHIVE})
